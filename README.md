33978d7a39559dd36176eec87cbf9b0ed767224e
Guilherme Batista <guilhermemalka6@gmail.com>
2022-03-30 17:51:08 -0300

    Criação da pagina de inserção de dados e da tabela que renderiza o valor inserido, assim como a criação do json rest API fake em um servidor local

--- README.md
+++ README.md
@@ -25,3 +25,13 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## =========================================================================
+
+## npm install -g json-server
+
+to install json server emulator
+
+## json-server --watch db.json
+
+to run json server with read db.json archive
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { NgModule } from '@angular/core';
+import { NovatransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt'
+
+registerLocaleData(localePt, 'pt')
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovatransferenciaComponent,
+    ExtratoComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt'},
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL',
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
